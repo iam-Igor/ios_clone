abf267c008b0b1713d0f6b242d42846976a587dc
Ygor Garofalo <igorgarofalo@gmail.com>
2023-11-27 00:58:19 +0100

    fixes

--- src/comps/AssistiveTouch.jsx
+++ src/comps/AssistiveTouch.jsx
@@ -1,10 +1,11 @@
 import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import Draggable from "react-draggable";
 import assistive from "../assets/unnamed.webp";
 
 const AssistiveTouch = () => {
   const location = useNavigate();
+  const navigate = useLocation();
   const [showmenu, setShowMenu] = useState(false);
   const [draggable, setDraggable] = useState(false);
 
@@ -34,39 +35,43 @@ const AssistiveTouch = () => {
       }}
       disabled={draggable}
     >
-      <div
-        className="fab-wrapper col-3 d-flex flex-column align-items-center"
-        style={{ position: "absolute" }}
-      >
-        <img
-          src={assistive}
-          style={{ width: "70%" }}
-          alt="Assistive Touch"
-          className="img"
-        />
+      {navigate.pathname === "/Lockscreen/" ? (
+        <div></div>
+      ) : (
         <div
-          className={`assistive-menu fs-1 text-white bg-dark justify-content-around d-flex px-2 py-3 rounded-4 mb-5 w-100 ${
-            showmenu ? "open" : "close"
-          }`}
+          className="fab-wrapper col-3 d-flex flex-column align-items-center"
+          style={{ position: "absolute" }}
         >
-          <i
-            className="bi bi-lock-fill"
-            onClick={() => {
-              setDraggable(false);
-              location("/Lockscreen/");
-              setShowMenu(false);
-            }}
-          ></i>
-          <i
-            className="bi bi-house-door-fill"
-            onClick={() => {
-              setDraggable(false);
-              location("/");
-              setShowMenu(false);
-            }}
-          ></i>
+          <img
+            src={assistive}
+            style={{ width: "70%" }}
+            alt="Assistive Touch"
+            className="img"
+          />
+          <div
+            className={`assistive-menu fs-1 text-white bg-dark justify-content-around d-flex px-2 py-3 rounded-4 mb-5 w-100 ${
+              showmenu ? "open" : "close"
+            }`}
+          >
+            <i
+              className="bi bi-lock-fill"
+              onClick={() => {
+                setDraggable(false);
+                location("/Lockscreen/");
+                setShowMenu(false);
+              }}
+            ></i>
+            <i
+              className="bi bi-house-door-fill"
+              onClick={() => {
+                setDraggable(false);
+                location("/");
+                setShowMenu(false);
+              }}
+            ></i>
+          </div>
         </div>
-      </div>
+      )}
     </Draggable>
   );
 };
